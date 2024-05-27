a9df6a0441ad057f66ede548ae6720b355144e67
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-05-27 23:36:02 +0330

    Swagger added

--- divar-clone/main.js
+++ divar-clone/main.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const swaggerConfig = require("./src/config/swagger.config");
+
 async function main() {
   const app = express();
   const PORT = process.env.PORT;
   require("./src/config/mongoose.config");
+  swaggerConfig(app);
 
   app.listen(PORT, () => {
     console.log(`server: http://localhost:${PORT}`);
