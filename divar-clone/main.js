99a1680b0c5e62e49d3a52fa0ef549b7e4d1640e
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-05-27 21:57:25 +0330

    MongoDB connection added

--- divar-clone/main.js
+++ divar-clone/main.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 async function main() {
   const app = express();
+  const PORT = process.env.PORT;
+  require("./src/config/mongoose.config");
 
-  app.listen(3000, () => {
-    console.log("server: http://localhost:3000");
+  app.listen(PORT, () => {
+    console.log(`server: http://localhost:${PORT}`);
   });
 }
 
---
+++ divar-clone/src/config/mongoose.config.js
@@ -0,0 +1,9 @@
+require("dotenv").config;
+const { default: mongoose } = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => console.log(err?.message ?? "DB connection failed"));
