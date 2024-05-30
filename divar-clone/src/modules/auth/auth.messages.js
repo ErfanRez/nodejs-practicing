3cb712a124d242d05718eca597e081be1a71b1b5
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-05-30 16:09:59 +0330

    auth module added

---
+++ divar-clone/src/modules/auth/auth.messages.js
@@ -0,0 +1,10 @@
+const AuthMessage = {
+  SendOtpSuccessfully: "sent otp successfully",
+  NotFound: "user not found!",
+  OtpCodeNotExpired: "otp code not expired please try later!",
+  OtpCodeExpired: "otp code expired please try again!",
+  IncorrectOtpCode: "Wrong otp code!",
+  loginSuccessfully: "You have logged in successfully",
+};
+
+module.exports = AuthMessage;
