0fc4dd75fb28f7ff81a511129884ad2781131c06
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-05-30 16:09:50 +0330

    auth module added

---
+++ divar-clone/src/modules/auth/auth.controller.js
@@ -0,0 +1,45 @@
+const AuthMessage = require("./auth.messages");
+const authService = require("./auth.service");
+const autoBind = require("auto-bind");
+
+class AuthController {
+  #service;
+  constructor() {
+    autoBind(this);
+    this.#service = authService;
+  }
+
+  async sendOTP(req, res, next) {
+    try {
+      const { mobile } = req.body;
+      await this.#service.sendOTP(mobile);
+      return res.json({
+        message: AuthMessage.SendOtpSuccessfully,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async checkOTP(req, res, next) {
+    try {
+      const { mobile, code } = req.body;
+      const token = await this.#service.checkOTP(mobile, code);
+      return res.json({
+        message: AuthMessage.loginSuccessfully,
+        token,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async Logout(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = new AuthController();
