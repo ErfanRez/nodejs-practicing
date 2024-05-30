47b25d928becb9e8159db7864de26c6ccbed534e
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-05-30 13:10:25 +0330

    API routes added

--- divar-clone/main.js
+++ divar-clone/main.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const swaggerConfig = require("./src/config/swagger.config");
+const mainRouter = require("./src/app.routes");
 
 async function main() {
   const app = express();
   const PORT = process.env.PORT;
   require("./src/config/mongoose.config");
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   swaggerConfig(app);
+  app.use(mainRouter);
 
   app.listen(PORT, () => {
     console.log(`server: http://localhost:${PORT}`);
