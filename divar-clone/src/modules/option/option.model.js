e313b177631ce341ccec1324606b46533f28c046
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-06-01 12:12:46 +0330

    Bugs fixed

--- divar-clone/src/modules/option/option.model.js
+++ divar-clone/src/modules/option/option.model.js
@@ -1,12 +1,12 @@
 const { Schema, model, Types } = require("mongoose");
 
-const OptionSchema = Schema({
+const OptionSchema = new Schema({
   title: { type: String, required: true },
   key: { type: String, required: true },
   type: { type: String, enum: ["number", "string", "array", "boolean"] },
   enum: { type: Array, default: [] },
   guide: { type: String, required: false },
-  category: { type: Types.ObjectId, ref: "Category", required: true },
+  category: { type: Types.ObjectId, ref: "category", required: true },
 });
 
 const OptionModel = model("option", OptionSchema);
